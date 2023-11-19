7f49790b6dfd93221da7048c2c35516537c3860f
oscarmorand <oscar.morand@epita.fr>
2023-11-19 17:41:07 +0100

    refacto code, add world to main

--- src/main.cc
+++ src/main.cc
@@ -1,26 +1,19 @@
-#include "color.hh"
-#include "vec3.hh"
+#include "raytracer.hh"
 
-int main() {
+#include "camera.hh"
+#include "hittable_list.hh"
+#include "sphere.hh"
 
-    // Image
+int main()
+{
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+    
+    camera cam;
 
-    int image_width = 256;
-    int image_height = 256;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
 
-    // Render
-
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; j++)
-    {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        
-        for (int i = 0; i < image_width; i++) {
-            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
-            write_color(std::cout, pixel_color);
-        }
-    }
-
-    std::clog << "\rDone.                     \n";
+    cam.render(world);
 }
