3c5ba24bb8d23a6133056800a2da25280c792db5
oscarmorand <oscar.morand@epita.fr>
2023-11-19 19:29:05 +0100

    antialiasing

--- src/raytracer.hh
+++ src/raytracer.hh
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <cmath>
+#include <cstdlib>
+#include <iostream>
 #include <limits>
 #include <memory>
-#include <iostream>
 
 // Usings
 
@@ -18,11 +19,24 @@ const double pi = 3.1415926535897932385;
 
 // Utility Functions
 
-inline double degrees_to_radians(double degrees) {
+inline double degrees_to_radians(double degrees)
+{
     return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    // Returns a random real in [0,1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+    // Returns a random real in [min,max)
+    return min + (max - min) * random_double();
 }
 
 // Common Headers
 
-#include "vec3.hh"
 #include "ray.hh"
+#include "vec3.hh"
